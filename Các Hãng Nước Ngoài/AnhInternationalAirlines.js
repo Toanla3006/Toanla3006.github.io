ce220b9a97a1ea1e91b8137b5c48d39c3d143168
Toanla3006 <55652183+Toanla3006@users.noreply.github.com>
2020-04-23 04:47:32 +0700

    AviationDream

---
+++ Các Hãng Nước Ngoài/AnhInternationalAirlines.js
@@ -0,0 +1,289 @@
+// JavaScript Document
+$(document).ready(function(){
+	XULITHONGTINTHEMES();
+	XULITHONGTINHINHANH();
+	XULITINHIEU();
+});
+
+var WebsiteName ="Đam mê Hàng Không";
+var SLOGAN = "Chấp cánh giấc mơ bay";
+var TieuDeChuyenMuc = "Chuyên Mục: Ảnh Hàng Không - Các hãng quốc tế";
+
+var HotPic1_address = "../Picture/IMG_0746.jpg";
+var HotPic2_address = "../Picture/IMG_3803.jpg";
+var HotPic3_address = "../Picture/IMG_2345.jpg";
+var HotPic4_address = "../Picture/IMG_7515.jpg";
+var AdvertisingPic  = "../Picture/AdvertiseHomePage.jpg";
+
+var AHK1 = "../Picture/Waiting_Pic.jpg";
+var AHK2 = "../Picture/Waiting_Pic.jpg";
+var AHK3 = "../Picture/Waiting_Pic.jpg";
+var AHK4 = "../Picture/Waiting_Pic.jpg";
+var AHK5 = "../Picture/Waiting_Pic.jpg";
+var AHK6 = "../Picture/Waiting_Pic.jpg";
+var AHK7 = "../Picture/Waiting_Pic.jpg";
+var AHK8 = "../Picture/Waiting_Pic.jpg";
+var AHK9 = "../Picture/Waiting_Pic.jpg";
+var AHK10 = "../Picture/Waiting_Pic.jpg";
+var AHK11 = "../Picture/Waiting_Pic.jpg";
+var AHK12 = "../Picture/Waiting_Pic.jpg";
+
+var AHK13 = "../Picture/Waiting_Pic.jpg";
+var AHK14 = "../Picture/Waiting_Pic.jpg";
+var AHK15 = "../Picture/Waiting_Pic.jpg";
+var AHK16 = "../Picture/Waiting_Pic.jpg";
+var AHK17 = "../Picture/Waiting_Pic.jpg";
+var AHK18 = "../Picture/Waiting_Pic.jpg";
+var AHK19 = "../Picture/Waiting_Pic.jpg";
+var AHK20 = "../Picture/Waiting_Pic.jpg";
+var AHK21 = "../Picture/Waiting_Pic.jpg";
+var AHK22 = "../Picture/Waiting_Pic.jpg";
+var AHK23 = "../Picture/Waiting_Pic.jpg";
+var AHK24 = "../Picture/Waiting_Pic.jpg";
+
+var AHK25 = "../Picture/Waiting_Pic.jpg";
+var AHK26 = "../Picture/Waiting_Pic.jpg";
+var AHK27 = "../Picture/Waiting_Pic.jpg";
+var AHK28 = "../Picture/Waiting_Pic.jpg";
+var AHK29 = "../Picture/Waiting_Pic.jpg";
+var AHK30 = "../Picture/Waiting_Pic.jpg";
+var AHK31 = "../Picture/Waiting_Pic.jpg";
+var AHK32 = "../Picture/Waiting_Pic.jpg";
+var AHK33 = "../Picture/Waiting_Pic.jpg";
+var AHK34 = "../Picture/Waiting_Pic.jpg";
+var AHK35 = "../Picture/Waiting_Pic.jpg";
+var AHK36 = "../Picture/Waiting_Pic.jpg";
+
+var AHK37 = "../Picture/Waiting_Pic.jpg";
+var AHK38 = "../Picture/Waiting_Pic.jpg";
+var AHK39 = "../Picture/Waiting_Pic.jpg";
+var AHK40 = "../Picture/Waiting_Pic.jpg";
+var AHK41 = "../Picture/Waiting_Pic.jpg";
+var AHK42 = "../Picture/Waiting_Pic.jpg";
+var AHK43 = "../Picture/Waiting_Pic.jpg";
+var AHK44 = "../Picture/Waiting_Pic.jpg";
+var AHK45 = "../Picture/Waiting_Pic.jpg";
+var AHK46 = "../Picture/Waiting_Pic.jpg";
+var AHK47 = "../Picture/Waiting_Pic.jpg";
+var AHK48 = "../Picture/Waiting_Pic.jpg";
+
+
+var ThongTinAHK1 = "Thông Tin Tàu Bay";
+var ThongTinAHK2 = "Thông Tin Tàu Bay";
+var ThongTinAHK3 = "Thông Tin Tàu Bay";
+var ThongTinAHK4 = "Thông Tin Tàu Bay";
+var ThongTinAHK5 = "Thông Tin Tàu Bay";
+var ThongTinAHK6 = "Thông Tin Tàu Bay";
+var ThongTinAHK7 = "Thông Tin Tàu Bay";
+var ThongTinAHK8 = "Thông Tin Tàu Bay";
+var ThongTinAHK9 = "Thông Tin Tàu Bay";
+var ThongTinAHK10 = "Thông Tin Tàu Bay";
+var ThongTinAHK11 = "Thông Tin Tàu Bay";
+var ThongTinAHK12 = "Thông Tin Tàu Bay";
+
+var ThongTinAHK13 = "Thông Tin Tàu Bay";
+var ThongTinAHK14 = "Thông Tin Tàu Bay";
+var ThongTinAHK15 = "Thông Tin Tàu Bay";
+var ThongTinAHK16 = "Thông Tin Tàu Bay";
+var ThongTinAHK17 = "Thông Tin Tàu Bay";
+var ThongTinAHK18 = "Thông Tin Tàu Bay";
+var ThongTinAHK19 = "Thông Tin Tàu Bay";
+var ThongTinAHK20 = "Thông Tin Tàu Bay";
+var ThongTinAHK21 = "Thông Tin Tàu Bay";
+var ThongTinAHK22 = "Thông Tin Tàu Bay";
+var ThongTinAHK23 = "Thông Tin Tàu Bay";
+var ThongTinAHK24 = "Thông Tin Tàu Bay";
+
+var ThongTinAHK25 = "Thông Tin Tàu Bay";
+var ThongTinAHK26 = "Thông Tin Tàu Bay";
+var ThongTinAHK27 = "Thông Tin Tàu Bay";
+var ThongTinAHK28 = "Thông Tin Tàu Bay";
+var ThongTinAHK29 = "Thông Tin Tàu Bay";
+var ThongTinAHK30 = "Thông Tin Tàu Bay";
+var ThongTinAHK31 = "Thông Tin Tàu Bay";
+var ThongTinAHK32 = "Thông Tin Tàu Bay";
+var ThongTinAHK33 = "Thông Tin Tàu Bay";
+var ThongTinAHK34 = "Thông Tin Tàu Bay";
+var ThongTinAHK35 = "Thông Tin Tàu Bay";
+var ThongTinAHK36 = "Thông Tin Tàu Bay";
+
+var ThongTinAHK37 = "Thông Tin Tàu Bay";
+var ThongTinAHK38 = "Thông Tin Tàu Bay";
+var ThongTinAHK39 = "Thông Tin Tàu Bay";
+var ThongTinAHK40 = "Thông Tin Tàu Bay";
+var ThongTinAHK41 = "Thông Tin Tàu Bay";
+var ThongTinAHK42 = "Thông Tin Tàu Bay";
+var ThongTinAHK43 = "Thông Tin Tàu Bay";
+var ThongTinAHK44 = "Thông Tin Tàu Bay";
+var ThongTinAHK45 = "Thông Tin Tàu Bay";
+var ThongTinAHK46 = "Thông Tin Tàu Bay";
+var ThongTinAHK47 = "Thông Tin Tàu Bay";
+var ThongTinAHK48 = "Thông Tin Tàu Bay";
+
+function XULITHONGTINTHEMES()
+{
+	document.getElementById('WebsiteName').innerHTML=WebsiteName;
+	document.getElementById('Slogan').innerHTML=SLOGAN;
+	document.getElementById('TieuDeChuyenMuc').innerHTML=TieuDeChuyenMuc;
+}
+
+function XULITHONGTINHINHANH()
+{
+	$('#HotPic1').attr("src",HotPic1_address);
+	$('#HotPic2').attr("src",HotPic2_address);
+	$('#HotPic3').attr("src",HotPic3_address);
+	$('#HotPic4').attr("src",HotPic4_address);
+	$('#AdvertisingPic').attr("src",AdvertisingPic);
+	PushUpPicturePage1();
+}
+function XULITINHIEU()
+{
+	var Button1 = document.getElementById('Button1');
+	Button1.onmouseover = PushUpPicturePage1;
+	var Button2 = document.getElementById('Button2');
+	Button2.onmouseover = PushUpPicturePage2;
+	var Button3 = document.getElementById('Button3');
+	Button3.onmouseover = PushUpPicturePage3;
+	var Button4 = document.getElementById('Button4');
+	Button4.onmouseover = PushUpPicturePage4;
+}
+
+function PushUpPicturePage1(){
+	$('#AHK1').attr("src",AHK1);
+	$('#AHK2').attr("src",AHK2);
+	$('#AHK3').attr("src",AHK3);
+	$('#AHK4').attr("src",AHK4);
+	$('#AHK5').attr("src",AHK5);
+	$('#AHK6').attr("src",AHK6);
+	$('#AHK7').attr("src",AHK7);
+	$('#AHK8').attr("src",AHK8);
+	$('#AHK9').attr("src",AHK9);
+	$('#AHK10').attr("src",AHK10);
+	$('#AHK11').attr("src",AHK11);
+	$('#AHK12').attr("src",AHK12);
+	document.getElementById('PicInfo1').innerHTML=ThongTinAHK1;
+	document.getElementById('PicInfo2').innerHTML=ThongTinAHK2;
+	document.getElementById('PicInfo3').innerHTML=ThongTinAHK3;
+	document.getElementById('PicInfo4').innerHTML=ThongTinAHK4;
+	document.getElementById('PicInfo5').innerHTML=ThongTinAHK5;
+	document.getElementById('PicInfo6').innerHTML=ThongTinAHK6;
+	document.getElementById('PicInfo7').innerHTML=ThongTinAHK7;
+	document.getElementById('PicInfo8').innerHTML=ThongTinAHK8;
+	document.getElementById('PicInfo9').innerHTML=ThongTinAHK9;
+	document.getElementById('PicInfo10').innerHTML=ThongTinAHK10;
+	document.getElementById('PicInfo11').innerHTML=ThongTinAHK11;
+	document.getElementById('PicInfo12').innerHTML=ThongTinAHK12;
+	document.getElementById('Button1').style.backgroundColor='white';
+	document.getElementById('Button1').style.color='black';
+	document.getElementById('Button2').style.backgroundColor='black';
+	document.getElementById('Button2').style.color='white';
+	document.getElementById('Button3').style.backgroundColor='black';
+	document.getElementById('Button3').style.color='white';
+	document.getElementById('Button4').style.backgroundColor='black';
+	document.getElementById('Button4').style.color='white';
+}
+
+function PushUpPicturePage2(){
+	$('#AHK1').attr("src",AHK13);
+	$('#AHK2').attr("src",AHK14);
+	$('#AHK3').attr("src",AHK15);
+	$('#AHK4').attr("src",AHK16);
+	$('#AHK5').attr("src",AHK17);
+	$('#AHK6').attr("src",AHK18);
+	$('#AHK7').attr("src",AHK19);
+	$('#AHK8').attr("src",AHK20);
+	$('#AHK9').attr("src",AHK21);
+	$('#AHK10').attr("src",AHK22);
+	$('#AHK11').attr("src",AHK23);
+	$('#AHK12').attr("src",AHK24);
+	document.getElementById('PicInfo1').innerHTML=ThongTinAHK13;
+	document.getElementById('PicInfo2').innerHTML=ThongTinAHK14;
+	document.getElementById('PicInfo3').innerHTML=ThongTinAHK15;
+	document.getElementById('PicInfo4').innerHTML=ThongTinAHK16;
+	document.getElementById('PicInfo5').innerHTML=ThongTinAHK17;
+	document.getElementById('PicInfo6').innerHTML=ThongTinAHK18;
+	document.getElementById('PicInfo7').innerHTML=ThongTinAHK19;
+	document.getElementById('PicInfo8').innerHTML=ThongTinAHK20;
+	document.getElementById('PicInfo9').innerHTML=ThongTinAHK21;
+	document.getElementById('PicInfo10').innerHTML=ThongTinAHK22;
+	document.getElementById('PicInfo11').innerHTML=ThongTinAHK23;
+	document.getElementById('PicInfo12').innerHTML=ThongTinAHK24;
+	document.getElementById('Button1').style.backgroundColor='black';
+	document.getElementById('Button1').style.color='white';
+	document.getElementById('Button2').style.backgroundColor='white';
+	document.getElementById('Button2').style.color='black';
+	document.getElementById('Button3').style.backgroundColor='black';
+	document.getElementById('Button3').style.color='white';
+	document.getElementById('Button4').style.backgroundColor='black';
+	document.getElementById('Button4').style.color='white';
+}
+
+function PushUpPicturePage3(){
+	$('#AHK1').attr("src",AHK25);
+	$('#AHK2').attr("src",AHK26);
+	$('#AHK3').attr("src",AHK27);
+	$('#AHK4').attr("src",AHK28);
+	$('#AHK5').attr("src",AHK29);
+	$('#AHK6').attr("src",AHK30);
+	$('#AHK7').attr("src",AHK31);
+	$('#AHK8').attr("src",AHK32);
+	$('#AHK9').attr("src",AHK33);
+	$('#AHK10').attr("src",AHK34);
+	$('#AHK11').attr("src",AHK35);
+	$('#AHK12').attr("src",AHK36);
+	document.getElementById('PicInfo1').innerHTML=ThongTinAHK25;
+	document.getElementById('PicInfo2').innerHTML=ThongTinAHK26;
+	document.getElementById('PicInfo3').innerHTML=ThongTinAHK27;
+	document.getElementById('PicInfo4').innerHTML=ThongTinAHK28;
+	document.getElementById('PicInfo5').innerHTML=ThongTinAHK29;
+	document.getElementById('PicInfo6').innerHTML=ThongTinAHK30;
+	document.getElementById('PicInfo7').innerHTML=ThongTinAHK31;
+	document.getElementById('PicInfo8').innerHTML=ThongTinAHK32;
+	document.getElementById('PicInfo9').innerHTML=ThongTinAHK33;
+	document.getElementById('PicInfo10').innerHTML=ThongTinAHK34;
+	document.getElementById('PicInfo11').innerHTML=ThongTinAHK35;
+	document.getElementById('PicInfo12').innerHTML=ThongTinAHK36;
+	document.getElementById('Button1').style.backgroundColor='black';
+	document.getElementById('Button1').style.color='white';
+	document.getElementById('Button2').style.backgroundColor='black';
+	document.getElementById('Button2').style.color='white';
+	document.getElementById('Button3').style.backgroundColor='white';
+	document.getElementById('Button3').style.color='black';
+	document.getElementById('Button4').style.backgroundColor='black';
+	document.getElementById('Button4').style.color='white';
+}
+
+function PushUpPicturePage4(){
+	$('#AHK1').attr("src",AHK37);
+	$('#AHK2').attr("src",AHK38);
+	$('#AHK3').attr("src",AHK39);
+	$('#AHK4').attr("src",AHK40);
+	$('#AHK5').attr("src",AHK41);
+	$('#AHK6').attr("src",AHK42);
+	$('#AHK7').attr("src",AHK43);
+	$('#AHK8').attr("src",AHK44);
+	$('#AHK9').attr("src",AHK45);
+	$('#AHK10').attr("src",AHK46);
+	$('#AHK11').attr("src",AHK47);
+	$('#AHK12').attr("src",AHK48);
+	document.getElementById('PicInfo1').innerHTML=ThongTinAHK37;
+	document.getElementById('PicInfo2').innerHTML=ThongTinAHK38;
+	document.getElementById('PicInfo3').innerHTML=ThongTinAHK39;
+	document.getElementById('PicInfo4').innerHTML=ThongTinAHK40;
+	document.getElementById('PicInfo5').innerHTML=ThongTinAHK41;
+	document.getElementById('PicInfo6').innerHTML=ThongTinAHK42;
+	document.getElementById('PicInfo7').innerHTML=ThongTinAHK43;
+	document.getElementById('PicInfo8').innerHTML=ThongTinAHK44;
+	document.getElementById('PicInfo9').innerHTML=ThongTinAHK45;
+	document.getElementById('PicInfo10').innerHTML=ThongTinAHK46;
+	document.getElementById('PicInfo11').innerHTML=ThongTinAHK47;
+	document.getElementById('PicInfo12').innerHTML=ThongTinAHK48;
+	document.getElementById('Button1').style.backgroundColor='black';
+	document.getElementById('Button1').style.color='white';
+	document.getElementById('Button2').style.backgroundColor='black';
+	document.getElementById('Button2').style.color='white';
+	document.getElementById('Button3').style.backgroundColor='black';
+	document.getElementById('Button3').style.color='white';
+	document.getElementById('Button4').style.backgroundColor='white';
+	document.getElementById('Button4').style.color='black';
+}
