b7f0d913705ce1a05a453b24d23d5f0524e47d18
Toanla3006 <55652183+Toanla3006@users.noreply.github.com>
2020-06-24 22:42:46 +0700

    Submit

---
+++ Sơ đồ bay VNA/SDBVNA.js
@@ -0,0 +1,30 @@
+// JavaScript Document
+$(document).ready(function(){
+	XULITHONGTINTHEMES();
+	XULITHONGTINHINHANH();
+});
+
+var WebsiteName ="Đam mê Hàng Không";
+var SLOGAN = "Chấp cánh giấc mơ bay";
+var TieuDeChuyenMuc = "Chuyên Mục: Sơ Đồ Bay - Vietnam Airlines (Chưa có Dữ Liệu)";
+
+var HotPic1_address = "../Picture/IMG_0746.jpg";
+var HotPic2_address = "../Picture/IMG_3803.jpg";
+var HotPic3_address = "../Picture/IMG_2345.jpg";
+var HotPic4_address = "../Picture/IMG_7515.jpg";
+var AdvertisingPic  = "../Picture/AdvertiseHomePage.jpg";
+function XULITHONGTINTHEMES()
+{
+	document.getElementById('WebsiteName').innerHTML=WebsiteName;
+	document.getElementById('Slogan').innerHTML=SLOGAN;
+	document.getElementById('TieuDeChuyenMuc').innerHTML=TieuDeChuyenMuc;
+}
+
+function XULITHONGTINHINHANH()
+{
+	$('#HotPic1').attr("src",HotPic1_address);
+	$('#HotPic2').attr("src",HotPic2_address);
+	$('#HotPic3').attr("src",HotPic3_address);
+	$('#HotPic4').attr("src",HotPic4_address);
+	$('#AdvertisingPic').attr("src",AdvertisingPic);
+}
